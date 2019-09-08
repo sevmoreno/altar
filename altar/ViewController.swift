d042743af2fa0b42f4acbf2dbc02dc8e9407ca36
Juan S Moreno <sevmoreno@gmail.com>
2019-09-08 10:49:33 -0700

    Initial Commit

---
+++ altar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  altar
+//
+//  Created by Juan Moreno on 9/8/19.
+//  Copyright © 2019 Juan Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
