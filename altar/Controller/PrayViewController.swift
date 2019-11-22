25a74a8a94cbda4349e56bc420c0554fd036beaa
Juan S Moreno <sevmoreno@gmail.com>
2019-11-22 12:20:24 -0800

    update with Dynamics

--- altar/Controller/PrayViewController.swift
+++ altar/Controller/PrayViewController.swift
@@ -14,6 +14,13 @@ class PrayViewController: UIViewController {
     @IBOutlet weak var feedCollection: UICollectionView!
     var posts = [Posts] ()
     
+    private let reuseIdentifier = "feedImage"
+    private let itemsPerRow: CGFloat = 1
+    private let sectionInsets = UIEdgeInsets(top: 50.0,
+                                             left: 20.0,
+                                             bottom: 50.0,
+                                             right: 20.0)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,7 +88,7 @@ class PrayViewController: UIViewController {
     
 
 }
-extension PrayViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension PrayViewController: UICollectionViewDelegate, UICollectionViewDataSource  {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
      return posts.count
@@ -157,5 +164,43 @@ extension PrayViewController: UICollectionViewDelegate, UICollectionViewDataSour
     }
     
     
+    
    
+}
+
+
+extension PrayViewController: UICollectionViewDelegateFlowLayout {
+    
+    // MARK: - Collection View Flow Layout Delegate
+
+        //1
+    
+    
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            sizeForItemAt indexPath: IndexPath) -> CGSize {
+            //2
+            let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+            let availableWidth = view.frame.width - paddingSpace
+            let widthPerItem = availableWidth / itemsPerRow
+            
+            return CGSize(width: widthPerItem, height: widthPerItem)
+        }
+        
+        //3
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            insetForSectionAt section: Int) -> UIEdgeInsets {
+            return sectionInsets
+        }
+        
+        // 4
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return sectionInsets.left
+        }
+
+    
+    
 }
